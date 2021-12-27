9a91ef6d6101cad425729968c5d79a9793906ae0
Isklimenkoisonya@gmail.com <Isklimenkoisonya@gmail.com>
2021-12-27 22:09:35 +0300

    Задание №8_updated

--- addressbook-web-tests/addressbook-web-tests/appmanager/ApplicationManager.cs
+++ addressbook-web-tests/addressbook-web-tests/appmanager/ApplicationManager.cs
@@ -25,7 +25,7 @@ namespace WebAddressbookTests
         private ApplicationManager()
         {
             driver = new FirefoxDriver();
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
             baseURL = "http://localhost";
 
             loginHelper = new LoginHelper(this);
